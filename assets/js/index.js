aaaf224d3155ede5f0813941d3ff7ad4580db4eb
Krishnanunni <krishnanunnir97@gmail.com>
2018-10-29 03:20:17 +0530

    [enhancement] Changed process from being on keydown to keyup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#markdown-textarea").keydown(function(){
+    $("#markdown-textarea").keyup(function(){
         var text = $('#markdown-textarea').val();
         $("#markdown-view").html(marked(text));
     });
