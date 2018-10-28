e221dab4423adff881701e3486c8ee610251f526
Krishnanunni <krishnanunnir97@gmail.com>
2018-10-29 02:25:44 +0530

    Added markdown processing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#markdown-textarea").keydown(function(){
         var text = $('#markdown-textarea').val();
-        $("#markdown-view").html("<p>"+text+"</p>")
+        $("#markdown-view").html(marked(text));
     });
  });
